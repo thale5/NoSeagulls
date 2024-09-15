7bbcae30627bc9647a782035e43a53241a7a67dd
thale5 <thale5@users.noreply.github.com>
2024-09-15 19:43:22 +0300

    Log Animal prefabs

--- NoSeagulls/PatchSeagulls.cs
+++ NoSeagulls/PatchSeagulls.cs
@@ -11,6 +11,7 @@ namespace NoSeagulls
         {
             try
             {
+                Util.DebugPrint("In Deploy");
                 threading.QueueSimulationThread(DeployImpl);
             }
             catch (Exception e)
@@ -24,6 +25,7 @@ namespace NoSeagulls
         {
             try
             {
+                Util.DebugPrint("In DeployImpl");
                 Patch();
             }
             catch (Exception e)
@@ -43,16 +45,34 @@ namespace NoSeagulls
             }
         }
 
+        // egrep -ri 'GetGroupAnimalInfo.+Garbage'
         static void Patch()
         {
             FastList<ushort>[] groupAnimals = (FastList<ushort>[]) Util.Get(Singleton<CitizenManager>.instance, "m_groupAnimals");
+            Util.DebugPrint("In Patch");
+            Util.DebugPrint("groupAnimals length", groupAnimals.Length.ToString());
 
+            int j = 0;
             foreach(FastList<ushort> list in groupAnimals)
+            {
                 if (list != null)
                 {
+                    ItemClass.Service service = ItemClass.Service.None;
+                    ItemClass.SubService sub = ItemClass.SubService.None;
+
+                    if (j + 1 < ItemClass.ServiceCount)
+                        service = (ItemClass.Service) (j + 1);
+                    else
+                        sub = (ItemClass.SubService) (j + 1 - ItemClass.ServiceCount);
+
                     int i = 0;
 
                     while(i < list.m_size)
+                    {
+                        var prefab = PrefabCollection<CitizenInfo>.GetPrefab(list[i]);
+                        Util.DebugPrint("Prefab", prefab.GetType().Name.ToString(), prefab.name,
+                            j.ToString(), i.ToString(), service.ToString(), sub.ToString());
+
                         if (IsSeagull(PrefabCollection<CitizenInfo>.GetPrefab(list[i])))
                         {
                             list.RemoveAt(i);
@@ -60,27 +80,26 @@ namespace NoSeagulls
                         }
                         else
                             i++;
+                    }
                 }
+                j++;
+            }
 
             int n = PrefabCollection<BuildingInfo>.PrefabCount();
 
             // Performance optimization.
             for (int i = 0; i < n; i++)
             {
-                switch (PrefabCollection<BuildingInfo>.GetPrefab((uint) i)?.GetAI())
-                {
-                    case HarborAI ai:
-                        ai.m_animalCount = 0;
-                        break;
+                PrefabAI ai = PrefabCollection<BuildingInfo>.GetPrefab((uint) i)?.GetAI();
 
-                    case CargoHarborAI ai:
-                        Util.Set(ai, "m_animalCount", 0);
-                        break;
-
-                    case LandfillSiteAI ai:
-                        ai.m_animalCount = 0;
-                        break;
-                }
+                if (ai is ParkAI p)
+                    p.m_seagulls = false;
+                else if (ai is HarborAI h)
+                    h.m_animalCount = 0;
+                else if (ai is CargoHarborAI c)
+                    Util.Set(c, "m_animalCount", 0);
+                else if (ai is LandfillSiteAI l)
+                    l.m_animalCount = 0;
             }
         }
 
@@ -89,6 +108,7 @@ namespace NoSeagulls
             CitizenManager cm = Singleton<CitizenManager>.instance;
             CitizenInstance[] buffer = cm.m_instances.m_buffer;
             int n = buffer.Length, count = 0;
+            Util.DebugPrint("In Release");
 
             for (int i = 1; i < n; i++)
                 if ((buffer[i].m_flags & CitizenInstance.Flags.Created) != CitizenInstance.Flags.None && IsSeagull(buffer[i].Info))
